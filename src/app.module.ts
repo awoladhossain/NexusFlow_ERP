4ad2ea58d468ab853dd32f61201335d3a42b0a29
awoladhossain <awoladh04@gmail.com>
2026-04-06 22:20:46 +0600

    infrastructure layer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './infrastructure/database/database.module';
 import { InventoryModule } from './modules/inventory/inventory.module';
 
 @Module({
-  imports: [InventoryModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Make the configuration available globally
+    }),
+    InventoryModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
